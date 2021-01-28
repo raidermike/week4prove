8cbc7ac007618d976c9e244a967358a2dbd1bb32
Aaron Shiffler <ajshiffler@gmail.com>
2021-01-28 12:08:42 -0700

    Aaron says hi to Michael

--- src/week03prove/Main.java
+++ src/week03prove/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Michael");
+        System.out.println("Hello Michael, my name is Aaron.");
     }
 }
